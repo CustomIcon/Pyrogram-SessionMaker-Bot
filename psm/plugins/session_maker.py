aea471fedace1ff97dfa22bbb1ce4d2d4d65227d
ポキ <aman_a@aol.com>
2020-10-23 00:29:15 +0500

    bugs fixed

--- psm/plugins/session_maker.py
+++ psm/plugins/session_maker.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, User, TermsOfService, ForceReply
 from pyrogram import Client
-from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait, PhoneCodeInvalid, PhoneNumberInvalid
 import asyncio
 
 from psm import psm, config
@@ -15,30 +15,40 @@ code_caches = dict()
 
 @psm.on_message(filters.command('phone'))
 async def phone_number(client, message):
-    await message.reply('Telegram will send you an activation code, Send it to me to get your session string', reply_markup=ForceReply(True))
     try:
-        phonenum = message.command[1]
+        phonenum = message.command[1].replace(' ', '')
     except IndexError:
-        await message.reply('Must pass args, example: `/phone +1234578900')
+        await message.reply('Must pass args, example: `/phone +1234578900`')
         return
+    await message.reply(
+        'Telegram will send you an activation code, Send it to me to get your session string',
+        reply_markup=ForceReply(True)
+        )
     await app.connect()
     try:
         sent_code = await app.send_code(phonenum)
     except FloodWait as e:
         await message.reply(f'I cannot create session for you.\nYou have a floodwait of: `{e.x}seconds``')
         return
-    await message.reply('send me your code in 20 seconds, make sure you reply to this message')
-    await asyncio.sleep(20)
-    signed_in = await app.sign_in(phonenum, sent_code.phone_code_hash, code_caches[message.from_user.id])
+    await message.reply('send me your code in 15 seconds, make sure you reply to this message')
+    await asyncio.sleep(15)
+    try:
+        signed_in = await app.sign_in(phonenum, sent_code.phone_code_hash, code_caches[message.from_user.id])
+    except PhoneCodeInvalid:
+        await message.reply('The code you sent seems Invalid, Try again.')
+        return
+    except PhoneNumberInvalid:
+        await message.reply('Phone number is invalid, Make sure you double check before sending.')
+        return
     if isinstance(signed_in, User):
         return signed_in
-    await message.reply(app.export_session_string())
+    a = await app.export_session_string()
+    await message.reply(a)
     await app.stop()
 
-@psm.on_message(filters.private)
+@psm.on_message(~filters.group, group=6)
 async def code_save(client, message):
     if message.reply_to_message:
         code_caches[message.from_user.id] = message.text
-        print(code_caches)
     else:
         return
--- psm/plugins/texts.py
+++ psm/plugins/texts.py
@@ -1,21 +1,26 @@
-helptext = "Deafult start message."
+helptext = """
+I am Pyrogram Session maker,
+
+I can make session strings for you on the go
+"""
 
 helptext1 = """
 **📖 Help (1 of 2)**
+you can send me your phone number with a command
 
-First
+`/phone (phone number with country code)`
 """
 
 helptext2 = """
 **📖 Help (2 of 2)**
 
-Second
+Telegram will send a code to you, with in 20 seconds make sure you send me the code
 """
 
 tiptext1 = """
 **💡 Tip (1 of 1)**
 
-First
+Theres no tip :)
 """
 
 default_inline = """

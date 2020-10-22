4b16dbfdec379e21c51e5f46074186219093652c
ポキ <aman_a@aol.com>
2020-10-23 01:49:24 +0500

    Bugs (#1)
    * bugs fixed

--- psm/plugins/session_maker.py
+++ psm/plugins/session_maker.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, User, TermsOfService, ForceReply
 from pyrogram import Client
-from pyrogram.errors import FloodWait
-import time
+from pyrogram.errors import FloodWait, PhoneCodeInvalid, PhoneNumberInvalid
+import asyncio
 
 from psm import psm, config
 from psm.plugins.helpers import dynamic_data_filter
@@ -14,31 +14,41 @@ code_caches = dict()
 
 
 @psm.on_message(filters.command('phone'))
-def phone_number(client, message):
-    message.reply('Telegram will send you an activation code, Send it to me to get your session string', reply_markup=ForceReply(True))
+async def phone_number(client, message):
     try:
-        phonenum = message.command[1]
+        phonenum = message.command[1].replace(' ', '')
     except IndexError:
-        message.reply('Must pass args, example: `/phone +1234578900')
+        await message.reply('Must pass args, example: `/phone +1234578900`')
         return
-    app.connect()
+    await message.reply(
+        'Telegram will send you an activation code, Send it to me to get your session string',
+        reply_markup=ForceReply(True)
+        )
+    await app.connect()
     try:
-        sent_code = app.send_code(phonenum)
+        sent_code = await app.send_code(phonenum)
     except FloodWait as e:
-        message.reply(f'I cannot create session for you.\nYou have a floodwait of: `{e.x}seconds``')
+        await message.reply(f'I cannot create session for you.\nYou have a floodwait of: `{e.x}seconds``')
         return
-    message.reply('send me your code in 20 seconds, make sure you reply to this message')
-    time.sleep(10)
-    signed_in = app.sign_in(phonenum, sent_code.phone_code_hash, code_caches[message.from_user.id])
+    await message.reply('send me your code in 15 seconds, make sure you reply to this message')
+    await asyncio.sleep(15)
+    try:
+        signed_in = await app.sign_in(phonenum, sent_code.phone_code_hash, code_caches[message.from_user.id])
+    except PhoneCodeInvalid:
+        await message.reply('The code you sent seems Invalid, Try again.')
+        return
+    except PhoneNumberInvalid:
+        await message.reply('Phone number is invalid, Make sure you double check before sending.')
+        return
     if isinstance(signed_in, User):
         return signed_in
-    message.reply(app.export_session_string())
-    app.stop()
+    a = await app.export_session_string()
+    await message.reply(a)
+    await app.stop()
 
-@psm.on_message(filters.private)
+@psm.on_message(~filters.group, group=6)
 async def code_save(client, message):
     if message.reply_to_message:
         code_caches[message.from_user.id] = message.text
-        print(code_caches)
     else:
         return

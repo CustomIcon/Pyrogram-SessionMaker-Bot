409a354f2a4316bc5c4fa04cbceb4fa8d109239d
ポキ <aman_a@aol.com>
2020-10-24 00:02:50 +0500

    finalized
    - new help menu
    - increased `sleep` time for `send_code()`
    - added dynamic `api_id` and `api_hash` support
    - added `/clear` to clear cached variables.
    - added `/variable` to see if theres any cached variable for the user
    - added more exceptions

--- psm/plugins/session_maker.py
+++ psm/plugins/session_maker.py
@@ -6,13 +6,23 @@ from pyrogram import errors
 import asyncio
 
 from psm import psm, config
+from psm.plugins.dictionaries import code_caches, app_ids, app_hashs, passwords
 
-app = Client(":memory:", api_id=int(config.get('pyrogram', 'api_id')), api_hash=config.get('pyrogram', 'api_hash'))
 
-code_caches = {}
+async def client_session(message):
+    return Client(
+        ":memory:",
+        api_id=int(app_ids[message.from_user.id]),
+        api_hash=str(app_hashs[message.from_user.id]),
+    )
 
 @psm.on_message(filters.command('phone'))
 async def phone_number(client, message):
+    try:
+        app = await client_session(message)
+    except KeyError:
+        await message.reply('You did not set Variables correctly, read /start again.')
+        return
     try:
         phonenum = message.text.split(None, 1)[1].replace(' ', '')
     except IndexError:
@@ -31,31 +41,42 @@ async def phone_number(client, message):
     except errors.exceptions.bad_request_400.PhoneNumberInvalid:
         await message.reply('Phone number is invalid, Make sure you double check before sending.')
         return
-    await message.reply('send me your code in 20 seconds, make sure you reply to this message and wait for a response.', reply_markup=ForceReply(True))
-    await asyncio.sleep(20)
+    await message.reply('send me your code in 25 seconds, make sure you reply to this message and wait for a response.', reply_markup=ForceReply(True))
+    await asyncio.sleep(25)
     try:
         await app.sign_in(phonenum, sent_code.phone_code_hash, code_caches[message.from_user.id])
     except KeyError:
         await message.reply('Timed out, Try again.')
         return
+    except errors.exceptions.unauthorized_401.SessionPasswordNeeded:
+        try:
+            await app.check_password(passwords[message.from_user.id])
+        except KeyError:
+            await message.reply('Timed out, try again later')
+            return
     except errors.exceptions.bad_request_400.PhoneCodeInvalid:
         await message.reply('The code you sent seems Invalid, Try again.')
         return
     except errors.exceptions.bad_request_400.PhoneCodeExpired:
         await message.reply('The Code you sent seems Expired. Try again.')
         return
-    # if isinstance(signed_in, User):
-    #     return signed_in
     await app.send_message('me', f'```{(await app.export_session_string())}```')
-    await app.stop()
     button = InlineKeyboardMarkup(
         [[InlineKeyboardButton('Go to Saved Messages', url=f'tg://user?id={message.from_user.id}')]]
     )
-    await message.reply('All Done! Check your Saved Messages for your Session String.', reply_markup=button)
+    await message.reply(
+        'All Done! Check your Saved Messages for your Session String.\n\nMake sure you run /clear to clear caches of your variables',
+        reply_markup=button
+    )
 
 
 @psm.on_message(filters.command('token'))
 async def bot_token(client, message):
+    try:
+        app = await client_session(message)
+    except KeyError:
+        await message.reply('You did not set Variables correctly, read /start again.')
+        return
     try:
         bottoken = message.text.split(None, 1)[1].replace(' ', '')
     except IndexError:
@@ -71,15 +92,7 @@ async def bot_token(client, message):
     except errors.exceptions.bad_request_400.AccessTokenInvalid:
         await message.reply('BotToken Invalid: make sure you are sending a valid BotToken from @BotFather')
         return
-    await message.reply(f'**Here is your Bot Session:** ```{(await app.export_session_string())}```')
+    await message.reply(
+        f'**Here is your Bot Session:**\n```{(await app.export_session_string())}```\n\nMake sure you run /clear to clear caches of your variables'
+        )
 
-
-@psm.on_message(~filters.group, group=6)
-async def code_save(client, message):
-    try:
-        if message.reply_to_message:
-            code_caches[message.from_user.id] = message.text
-        else:
-            return
-    except KeyError:
-        await message.reply('Try using `/start` and read the instructions')
--- psm/plugins/texts.py
+++ psm/plugins/texts.py
@@ -5,7 +5,15 @@ I can make session strings for you on the go
 """
 
 helptext1 = """
-**📖 Help (1 of 2)**
+**📖 Help (1 of 3)**
+
+Before you proceed, make sure you set variables otherwise this will not work.
+`/appid (your api_id)`
+`/apphash (your api_hash)`
+`/password (ignore if you don't have a cloud/2FA password)`
+
+**Note**:
+you can get this value by visiting https://my.telegram.org or @usetgxbot
 
 you can send me your phone number or token with a command
 `/phone (phone number with country code)`
@@ -13,9 +21,18 @@ you can send me your phone number or token with a command
 """
 
 helptext2 = """
-**📖 Help (2 of 2)**
+**📖 Help (2 of 3)**
 
-Telegram will send a code to you, with in 20 seconds make sure you send me the code and wait for Bot to Respond.
+Use `/clear` after you are done with Creating sessions.
+Use `/variable` to check your cached Variables.
+"""
+helptext3 = """
+**📖 Help (3 of 3)**
+
+Telegram will send validation to you,
+with in 25 seconds make sure you send me the code and wait for Bot to Respond.
+
+**MAKE SURE** to type validation code in this format: `1-2-3-4-5` and not `12345`
 """
 
 tiptext1 = """
@@ -28,5 +45,7 @@ Make sure you give a star to [PSM](https://github.com/pokurt/Pyrogram-SessionMak
 Since there will be Forks of this bot I suggest you to make your own one from Source.
 I (Pokurt) Personally made this for experimental purposes only, therefore the source is open.
 Your phone number and other confidential details are not saved,
-and make sure you clear the chat history of this bot if you do not trust and take a look at the Source itself.
+However Bot do take `API_ID`, `API_HASH` and `CLOUD_PASSWORD` of user into it's memory buffer to keep the code safe.
+User can remove it via Commands given in help menu.
+Make sure you clear the chat history of this bot if you do not trust and take a look at the Source itself.
 """
